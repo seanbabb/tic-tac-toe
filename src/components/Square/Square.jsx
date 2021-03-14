942c9cb27608044ef7079fcc62fc3ad04dcc1724
seanbabb <sbabb.nj@gmail.com>
2021-03-14 19:03:47 -0400

    passing data through props

--- src/components/Square/Square.jsx
+++ src/components/Square/Square.jsx
@@ -2,10 +2,19 @@ import React, { Component } from 'react';
 import '../Square/Square.css';
 
 export class Square extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            value: null,
+        };
+    }
     render() {
         return (
-            <button className="square">
-                {this.props.value}
+            <button 
+                className="square" 
+                onClick={() => this.setState({value: 'X'})}
+            >
+                {this.state.value}
             </button>
         );
     }
