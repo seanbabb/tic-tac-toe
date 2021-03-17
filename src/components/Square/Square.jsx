b6387029f51e13adcd4406d47a0f8f93f0da8e6b
seanbabb <sbabb.nj@gmail.com>
2021-03-16 22:54:00 -0400

    changed Square class component to function component

--- src/components/Square/Square.jsx
+++ src/components/Square/Square.jsx
@@ -1,18 +1,15 @@
-import React, { Component } from 'react';
+import React from 'react';
 import '../Square/Square.css';
 
-export class Square extends Component {
-   
-    render() {
-        return (
-            <button 
-                className="square" 
-                onClick={() => this.props.onClick()}
-            >
-                {this.props.value}
-            </button>
-        );
-    }
+function Square (props) {
+    return (
+        <button 
+            className="square" 
+            onClick={props.onClick}
+        >
+            {props.value}
+        </button>
+    );
 }
 
 export default Square
