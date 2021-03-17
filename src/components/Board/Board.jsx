656111a7c9f50b37542a3c572fe98f000e2b6595
seanbabb <sbabb.nj@gmail.com>
2021-03-16 23:02:47 -0400

    enabled taking turns

--- src/components/Board/Board.jsx
+++ src/components/Board/Board.jsx
@@ -7,13 +7,17 @@ export class Board extends Component {
         super(props);
         this.state = {
             squares: Array(9).fill(null),
+            xIsNext: true,
         };
     }
 
     handleClick(i) {
         const squares = this.state.squares.slice();
-        squares[i] = 'X';
-        this.setState({squares: squares});
+        squares[i] = this.state.xIsNext ? 'X' : 'O';
+        this.setState({
+            squares: squares,
+            xIsNext: !this.state.xIsNext,
+        });
     }
 
     renderSquare(i) {
@@ -26,7 +30,7 @@ export class Board extends Component {
     }
 
     render() {
-        const status = 'Next player: X';
+        const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
 
         return (
             <div>
